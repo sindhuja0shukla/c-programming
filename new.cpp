c122d9f620c38bfa6855303bf4c42a645ae52719
sony <sagarshukla100@gmail.com>
2019-10-14 13:48:01 +0530

    add two numbers

---
+++ new.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h.>
+#include<conio.h>
+int main()
+{
+	int a,b,c,subs;
+	a=10;
+	b=20;
+	c=5;
+	subs=a-b-c;
+printf("a=%d\n",a);
+printf("b=%d\n",b );
+printf("c=%d\n",c );
+printf("subs=%d\n",subs );
+	return 0;
+}
+
