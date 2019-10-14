d7c9664c7e2cc7d864e8429d4a64d41511fb155d
sony <sagarshukla100@gmail.com>
2019-10-14 13:35:22 +0530

    c program multiplication two numbers

---
+++ new.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,multi;
+	a=15;
+	b=10;
+	multi=a*b;
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+	printf("multi=%d\n",multi);
+	return 0; 
+}
