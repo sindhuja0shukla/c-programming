efb89e1059a7b1033f85ec96653392095c11e201
sony <sagarshukla100@gmail.com>
2019-10-15 15:52:31 +0530

    add any four numbers

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,d,sum;
+	a=10;
+	b=15;
+	c=25;
+	d=12;
+	sum=a+b+c+d;
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+	printf("c=%d\n",c);
+	printf("d=%d\n",d);
+	printf("add=%d\n",sum);
+	return 0;
+}
